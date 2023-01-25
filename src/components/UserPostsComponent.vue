415b9e3d10a012dbaa45baf11a3ce3ed69338068
Carlos <c.ochoa1993@live.com>
2023-01-25 11:36:34 -0800

    Added front end user post component
    This will be the starting point for messaging. We need to build the back-end

---
+++ src/components/UserPostsComponent.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="row q-pa-md items-start q-gutter-md">
+    <q-card class="my-card" flat bordered>
+      <q-card-section horizontal>
+        <q-card-section class="q-pt-xs">
+          <div class="text-overline">{{ date }}</div>
+          <div class="text-h5 q-mt-sm q-mb-xs">{{ userName }}</div>
+          <div class="text-caption text-grey">
+            {{ postText }}
+          </div>
+        </q-card-section>
+
+        <!-- <q-card-section class="col-5 flex flex-center">
+          <q-img
+            class="rounded-borders"
+            src="https://cdn.quasar.dev/img/parallax2.jpg"
+          />
+        </q-card-section> -->
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-input v-model="userPost" filled autogrow clearable
+          ><q-btn round dense flat icon="send" @click="dothings"></q-btn
+        ></q-input>
+      </q-card-actions>
+    </q-card>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "UserPostsComponent",
+  props: {
+    userName: {
+      type: String,
+      required: true,
+    },
+
+    date: {
+      type: String,
+      required: true,
+    },
+
+    postText: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {
+    const userPost = ref("");
+    return {
+      userPost,
+
+      dothings() {
+        console.log(userPost.value);
+        userPost.value = "";
+      },
+    };
+  },
+});
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,80 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="grid">
+    <div class="row col justify-center">
+      <q-card class="my-card q-pa-md q-ma-md" flat bordered>
+        <q-card-section horizontal>
+          <q-icon
+            name="record_voice_over"
+            class="q-pa-sm justify-center"
+          ></q-icon>
+          <q-input
+            v-model="userPost"
+            filled
+            autogrow
+            clearable
+            placeholder="How are you feeling today?"
+            ><q-btn round dense flat icon="send" @click="sendUserPost"></q-btn
+          ></q-input>
+        </q-card-section>
+      </q-card>
+    </div>
+    <div class="row col justify-center">
+      <q-list>
+        <UserPostsComponent
+          v-for="post in followerPosts"
+          :key="post.postText"
+          v-bind="post"
+        />
+      </q-list>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from "vue";
+import UserPostsComponent from "components/UserPostsComponent.vue";
+
+const followerPostsList = [
+  {
+    userName: "Carlos Ochoa",
+    date: "2023-01-24",
+    postText: "I really want to land this job!",
+  },
+  {
+    userName: "Jordan Ochoa",
+    date: "2023-01-24",
+    postText: "I wish Carlos would stop talking about wanting a job....",
+  },
+  {
+    userName: "Nugget Ochoa",
+    date: "2023-01-23",
+    postText: "I really want to fk up Noah!!!!",
+  },
+  {
+    userName: "Veronica Ochoa",
+    date: "2023-01-22",
+    postText: "Umm Umm Ummmmmmmmmmm",
+  },
+];
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+
+  components: {
+    UserPostsComponent,
+  },
+
+  setup() {
+    const userPost = ref("");
+    return {
+      userPost,
+      followerPosts: followerPostsList,
+
+      sendUserPost() {
+        console.log(userPost.value);
+        userPost.value = "";
+      },
+    };
+  },
+});
 </script>
